5449354c0a5dbe4e53834e3653f68b48dadb03c1
obijuan <obijuan.cube@gmail.com>
2025-07-16 11:50:42 +0200

    add instruction lui and tests

--- icerv-dasm/src/main.rs
+++ icerv-dasm/src/main.rs
@@ -24,6 +24,7 @@ const FIELD_5B: u32 = 0x1F;  //-- Campo de 5 bits
 const FIELD_6B: u32 = 0x3F;  //-- Campo de 6 bits
 const FIELD_7B: u32 = 0x7F;  //-- Campo de 7 bits
 const FIELD_12B: u32 = 0xFFF;  //-- Campo de 12 bits
+const FIELD_20B: u32 = 0xFFFFF; //-- Campo de 20 bits
 //────────────────────────────────────────────────
 //  POSICIONES de LOS CAMPOS
 //────────────────────────────────────────────────
@@ -33,7 +34,8 @@ const FUNC3_POS: u8 = 12;
 const RS1_POS: u8 = 15;  
 const RS2_POS: u8 = 20;  
 const FUNC7_POS: u8 = 25;  
-const IMM12_POS: u8 = 20;  
+const IMM12_POS: u8 = 20; 
+const IMM20_POS: u8 = 12; 
 const OFFSET7_POS: u8 = 25;
 const OFFSET6_POS: u8 = 25;
 const OFFSET5_POS: u8 = 7;
@@ -56,6 +58,7 @@ const RS1_MASK: u32 = FIELD_5B << RS1_POS;
 const RS2_MASK: u32 = FIELD_5B << RS2_POS;
 const FUNC7_MASK: u32 = FIELD_7B << FUNC7_POS;
 const IMM12_MASK: u32 = FIELD_12B << IMM12_POS;  
+const IMM20_MASK: u32 = FIELD_20B << IMM20_POS; 
 const OFFSET7_MASK: u32 = FIELD_7B << OFFSET7_POS;
 const OFFSET6_MASK: u32 = FIELD_6B << OFFSET6_POS;
 const OFFSET5_MASK: u32 = FIELD_5B << OFFSET5_POS;
@@ -78,7 +81,10 @@ const OPCODE_R: u32 = 0b_01100_11;  //-- 0x33
 const OPCODE_S: u32 = 0b_01000_11;  //-- 0x23
 //  Instrucciones tipo-B
 //────────────────────────────
-const OPCODE_B: u32 = 0b_11000_11;  //-- 0x63
+const OPCODE_B: u32 = 0b_11000_11; //-- 0x63
+//  Instruccion tipo-U: LUI
+//────────────────────────────
+const OPCODE_U_LUI: u32 = 0b_01101_11; //--0x37 
 
 
 fn get_opcode(inst: u32) -> u32 {
@@ -143,6 +149,20 @@ fn get_imm12(inst: u32) -> i32 {
   //-- Convertir el valor a i32 para manejar el signo
   //-- y devolverlo!
   sign_ext(imm12 as i32)
+}
+
+fn get_imm20(inst: u32) -> i32 {
+//────────────────────────────────────────────────
+// Entrada: Instrucción RISC-V
+// Salida: Inmediato de 20 bits de la instrucción
+//────────────────────────────────────────────────
+  //-- Aplicar la máscara para extraer el campo
+  //-- y desplazarlo a la posición 0
+  let imm20: u32 = (inst & IMM20_MASK) >> IMM20_POS;
+
+  //-- Convertir el valor a i32
+  //-- Hay que extender el signo
+  sign_ext20(imm20 as i32)
 }
 
 fn get_func7(inst: u32) -> u32 {
@@ -248,6 +268,25 @@ fn sign_ext13(value: i32) -> i32 {
     }
 }
 
+fn sign_ext20(value: i32) -> i32 {
+//────────────────────────────────────────────────
+// Entrada: Valor de 20 bits  
+// Salida: Valor extendido a 32 bits con signo
+//────────────────────────────────────────────────
+    //-- Obtener el bit de signo
+    //-- sign_bit = true --> negativo
+    let sign_bit = (value & 0x80000) != 0;
+
+    //-- En caso de ser negativo, extender el signo
+    if sign_bit {
+        value | !0xFFFFF  //-- Extender el signo a 32 bits
+    } else {
+        value  //-- No es negativo, devolver el valor original
+    }
+}
+
+
+
 fn is_type_i(opcode: u32) -> bool {
 //────────────────────────────────────────────────
 // Entrada: opcode RISC-V
@@ -277,6 +316,15 @@ fn is_type_i_load(opcode: u32) -> bool {
     }
     else {
       false
+    }
+}
+
+fn is_type_u_lui(opcode: u32) -> bool {
+    if opcode == OPCODE_U_LUI {
+        true
+    }
+    else {
+        false
     }
 }
 
@@ -547,10 +595,16 @@ fn disassemble(inst: u32) -> String {
         let offset: i32 = get_offset_b(inst);
 
         format!("{} x{}, x{}, {}", name, rs1, rs2, offset)
-    } else {
-      println!("   - Instrucción: DESCONOCIDA");
-      print_fields(inst);
-      String::from("DESCONOCIDA")
+    } else if is_type_u_lui(opcode) {
+
+        let imm20: i32 = get_imm20(inst);
+        let rd = get_rd(inst);
+        format!("lui x{}, {:#07X}", rd, imm20 & 0xFFFFF)
+    }
+    else {
+        println!("   - Instrucción: DESCONOCIDA");
+        print_fields(inst);
+        String::from("DESCONOCIDA")
     }
 }
 
@@ -598,31 +652,31 @@ fn main() {
         0x0083dc63, // bge x7, x8, 24
         0x00a4ea63, // bltu x9, x10, 20
         0x00c5f863, // bgeu x11, x12, 16
-
+        0x80000337, // lui x6, 0x80000
 ];
 
     //-- TODO
     //----- Tipo U
-    //-- lui
-    //-- auipc
+    //-- auipc: 0b_00101_11
     //----- Tipo J
-    //-- jal
-    //-- jalr
-    //----- Otros
-    //-- fence
-    //-- fence.i
+    //-- jal:   0b_11011_11
+    //-- jalr:  0b_11001_11
+    //----- Llamadas al sistema
+    //-- ecall
+    //-- ebreak
+    //----- Modo privilegiado
     //-- csrrw
     //-- csrrs
     //-- csrrc
     //-- csrrwi
     //-- csrrsi
     //-- csrrci
-    //-- ecall
-    //-- ebreak
     //-- uret
     //-- sret
     //-- mret
     //-- wfi
+    //-- fence
+    //-- fence.i
     //-- sfence.vma
     //----RV64I
     //-- addiw
@@ -1286,3 +1340,14 @@ fn test_disassemble_bgeu() {
     assert_eq!(disassemble(0x0128f263), "bgeu x17, x18, 4"); 
 }
 
+#[test]
+fn test_disassemble_lui() {
+    assert_eq!(disassemble(0x00000037), "lui x0, 0x00000");
+    assert_eq!(disassemble(0x000010b7), "lui x1, 0x00001");
+    assert_eq!(disassemble(0x00020137), "lui x2, 0x00020");
+    assert_eq!(disassemble(0x004001b7), "lui x3, 0x00400");
+    assert_eq!(disassemble(0x08000237), "lui x4, 0x08000");
+    assert_eq!(disassemble(0x7ffff2b7), "lui x5, 0x7FFFF");
+    assert_eq!(disassemble(0x80000337), "lui x6, 0x80000");
+    assert_eq!(disassemble(0xfffff3b7), "lui x7, 0xFFFFF");
+}
