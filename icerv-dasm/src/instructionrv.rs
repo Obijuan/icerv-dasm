9f2d581e38712a6541c6e5f7f680f5493231babd
obijuan <obijuan.cube@gmail.com>
2025-08-12 12:05:05 +0200

    instructionrv: implement ld

--- icerv-dasm/src/instructionrv.rs
+++ icerv-dasm/src/instructionrv.rs
@@ -647,6 +647,15 @@ impl InstructionRV {
                 //-- Devolver el codigo maquina como numero
                 mcode.value
             }
+            Self::Ld { rd, offs, rs1} => {
+                //-- Construir el código máquina
+                let mcode = MCode::new_typei_load(
+                    0b_011, *rd as u32, *rs1 as u32,
+                    *offs as u32);
+
+                //-- Devolver el codigo maquina como numero
+                mcode.value
+            }
             _ => 0
         }
     }
@@ -3715,4 +3724,35 @@ fn test_mcode2_lw() {
     assert_eq!(
         InstructionRV::Lw{ rd: Reg::X9, offs: 2047, rs1: Reg::X9}.to_mcode(),
         0x7ff4a483);
+}
+
+#[test]
+fn test_mcode2_ld() {
+    assert_eq!(
+        InstructionRV::Ld{rd: Reg::X0, offs: 0, rs1: Reg::X1}.to_mcode(), 
+        0x0000b003);
+    assert_eq!(
+        InstructionRV::Ld{rd: Reg::X1, offs: 1, rs1: Reg::X2}.to_mcode(), 
+        0x00113083);
+    assert_eq!(
+        InstructionRV::Ld{rd: Reg::X2, offs: 2, rs1: Reg::X3}.to_mcode(), 
+        0x0021b103);
+    assert_eq!(
+        InstructionRV::Ld{rd: Reg::X4, offs: 4, rs1: Reg::X4}.to_mcode(), 
+        0x00423203);
+    assert_eq!(
+        InstructionRV::Ld{rd: Reg::X5, offs: 8, rs1: Reg::X5}.to_mcode(), 
+        0x0082b283);
+    assert_eq!(
+        InstructionRV::Ld{rd: Reg::X6, offs: -1, rs1: Reg::X6}.to_mcode(), 
+        0xfff33303);
+    assert_eq!(
+        InstructionRV::Ld{rd: Reg::X7, offs: -2048, rs1: Reg::X7}.to_mcode(), 
+        0x8003b383);
+    assert_eq!(
+        InstructionRV::Ld{rd: Reg::X8, offs: -2, rs1: Reg::X8}.to_mcode(), 
+        0xffe43403);
+    assert_eq!(
+        InstructionRV::Ld{rd: Reg::X9, offs: 2047, rs1: Reg::X9}.to_mcode(), 
+        0x7ff4b483);
 }
